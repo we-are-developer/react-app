749518f2679c2bb0a8140b8608c3fb9855d90954
sonu_coder <chohansonu8796@gmail.com>
2020-01-19 08:28:17 +0530

    first commit

--- App.js
+++ App.js
@@ -7,19 +7,128 @@
  */
 import 'react-native-gesture-handler';
 
-import React from 'react';
-import {View, Text} from 'react-native';
+import React,{ Component }  from 'react';
+import {View, Text,Image, TouchableOpacity} from 'react-native';
 
-import { createAppContainer,createStackNavigator } from 'react-navigation';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import SafeAreaView from 'react-native-safe-area-view';
+
+import {createAppContainer} from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import {createStackNavigator} from 'react-navigation-stack';
+
+
+import Screen1 from '././components/Screen1';
+import Screen2 from '././components/Screen2';
+import Screen3 from '././components/Screen3';
+
+class NavigationDrawerStructure extends Component {
+  //Structure for the navigatin Drawer
+  toggleDrawer = () => {
+    //Props to open/close the drawer
+    this.props.navigationProps.toggleDrawer();
+  };
+  render() {
+    return (
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity onPress={this.toggleDrawer.bind(this)}>
+          {/*Donute Button Image */}
+          <Image
+            source={{uri: 'asset:/menu.png'}} 
+            style={{ width: 25, height: 25, marginLeft: 5 }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+ 
+const FirstActivity_StackNavigator = createStackNavigator({
+  //All the screen from the Screen1 will be indexed here
+  First: {
+    screen: Screen1,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Demo Screen 1',
+      headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
+      headerStyle: {
+        backgroundColor: '#FF9800',
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+});
+ 
+const Screen2_StackNavigator = createStackNavigator({
+  //All the screen from the Screen2 will be indexed here
+  Second: {
+    screen: Screen2,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Demo Screen 2',
+      headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
+      headerStyle: {
+        backgroundColor: '#FF9800',
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+});
+ 
+const Screen3_StackNavigator = createStackNavigator({
+  //All the screen from the Screen3 will be indexed here
+  Third: {
+    screen: Screen3,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Demo Screen 3',
+      headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
+      headerStyle: {
+        backgroundColor: '#FF9800',
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+});
+ 
+const DrawerNavigatorExample = createDrawerNavigator({
+  //Drawer Optons and indexing
+  Screen1: {
+    //Title
+    screen: FirstActivity_StackNavigator,
+    navigationOptions: {
+      drawerLabel: 'Demo Screen 1',
+    },
+  },
+  Screen2: {
+    //Title
+    screen: Screen2_StackNavigator,
+    navigationOptions: {
+      drawerLabel: 'Demo Screen 2',
+    },
+  },
+  Screen3: {
+    //Title
+    screen: Screen3_StackNavigator,
+    navigationOptions: {
+      drawerLabel: 'Demo Screen 3',
+    },
+  },
+});
+
+
+const MyApp = createAppContainer(DrawerNavigatorExample);
 
 
 
 class App extends React.Component {
   render() {
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Hello, world!</Text>
-      </View>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <MyApp/>
+        </View>
+        </SafeAreaView>
+        </SafeAreaProvider>
     );
   }
 }
